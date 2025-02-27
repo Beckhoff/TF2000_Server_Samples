10cdca1ba13d49d3257125cc7259cc81fb8d7264
Jonas Petereit <J.Petereit@beckhoff.com>
2025-02-27 10:22:53 +0100

    Extensions/RandomValue: simplify extension sample
    Remove the MaxRandom and MaxRandomFromConfig symbols so that the
    extension always gets the maximum random value from its configuration.

--- Extensions/RandomValue/CSharpRandomValue/CSharpRandomValue.cs
+++ Extensions/RandomValue/CSharpRandomValue/CSharpRandomValue.cs
@@ -10,7 +10,6 @@ using TcHmiSrv.Core.General;
 using TcHmiSrv.Core.Listeners;
 using TcHmiSrv.Core.Listeners.RequestListenerEventArgs;
 using TcHmiSrv.Core.Tools.Management;
-using ValueType = TcHmiSrv.Core.ValueType;
 
 namespace RandomValue
 {
@@ -21,17 +20,11 @@ namespace RandomValue
         private readonly Random _rand = new Random();
         private readonly RequestListener _requestListener = new RequestListener();
 
-        private Data _data;
-
         // initializes the TwinCAT HMI server extension
         public ErrorValue Init()
         {
             try
             {
-                // initialize data from configuration
-                // this can not be done before Init, because the properties of TcHmiApplication are initialized just before 
-                _data = new Data(TcHmiApplication.AsyncHost.GetConfigValue(TcHmiApplication.Context, "maxRandom"));
-
                 // add event handlers
                 _requestListener.OnRequest += OnRequest;
 
@@ -58,12 +51,6 @@ namespace RandomValue
                         case "RandomValue":
                             NextRandomValue(command);
                             break;
-                        case "MaxRandom":
-                            MaxRandom(command);
-                            break;
-                        case "MaxRandomFromConfig":
-                            MaxRandomFromConfig(command);
-                            break;
                     }
                 }
                 catch (Exception ex)
@@ -79,26 +66,7 @@ namespace RandomValue
         // generates a random value and writes it to the read value of the specified command
         private void NextRandomValue(Command command)
         {
-            command.ReadValue = _rand.Next(_data.MaxRandom) + 1;
-            command.ExtensionResult = Convert.ToUInt32(ExtensionSpecificError.Success);
-        }
-
-        // gets or sets the maximum random value
-        private void MaxRandom(Command command)
-        {
-            if (command.WriteValue != null && command.WriteValue.Type == ValueType.Int32)
-            {
-                _data.MaxRandom = command.WriteValue;
-            }
-
-            command.ReadValue = _data.MaxRandom;
-            command.ExtensionResult = Convert.ToUInt32(ExtensionSpecificError.Success);
-        }
-
-        // gets the maximum random value from the configuration of the TwinCAT HMI server extension
-        private void MaxRandomFromConfig(Command command)
-        {
-            command.ReadValue = TcHmiApplication.AsyncHost.GetConfigValue(TcHmiApplication.Context, "maxRandom");
+            command.ReadValue = _rand.Next(TcHmiApplication.AsyncHost.GetConfigValue(TcHmiApplication.Context, "maxRandom")) + 1;
             command.ExtensionResult = Convert.ToUInt32(ExtensionSpecificError.Success);
         }
     }
--- Extensions/RandomValue/README.md
+++ Extensions/RandomValue/README.md
@@ -3,17 +3,13 @@
 The "RandomValue" sample extension is a server extension that provides random
 numbers between zero and a configurable maximum.
 
-The current maximum can be read/set using the `RandomValue.MaxRandom` symbol.
 The initial maximum is 1000. New random numbers are returned by the
 `RandomValue.RandomValue` symbol and are always smaller or equal to
-`RandomValue.MaxRandom`.
+`RandomValue.Config::maxRandom`.
 
 The extension configuration includes a single integer value
 (`RandomValue.Config::maxRandom`) that can be read directly from the extension
-configuration or using the `RandomValue.MaxRandomFromConfig` symbol.
-`RandomValue.MaxRandom` and `RandomValue.Config::maxRandom` are never synced
-automatically. The value in the extension configuration is only used as a
-persistent storage.
+configuration.
 
 **First steps:**
 
@@ -46,96 +42,6 @@ persistent storage.
             {
                 "symbol": "RandomValue.RandomValue",
                 "readValue": 32
-            }
-        ]
-    }
-    ```
-
-1. Get the currently configured maximum using the `RandomValue.MaxRandom`
-symbol
-
-    **Request:**
-
-    ```json
-    {
-        "requestType": "ReadWrite",
-        "commands": [
-            {
-                "symbol": "RandomValue.MaxRandom",
-                "commandOptions": [ "SendErrorMessage" ]
-            }
-        ]
-    }
-    ```
-
-    **Response:**
-
-    ```json
-    {
-        "commands": [
-            {
-                "symbol": "RandomValue.MaxRandom",
-                "readValue": 1000
-            }
-        ]
-    }
-    ```
-
-1. Change the current maximum using the `RandomValue.MaxRandom` symbol
-
-    **Request:**
-
-    ```json
-    {
-        "requestType": "ReadWrite",
-        "commands": [
-            {
-                "symbol": "RandomValue.MaxRandom",
-                "writeValue": 1234,
-                "commandOptions": [ "SendErrorMessage" ]
-            }
-        ]
-    }
-    ```
-
-    **Response:**
-
-    ```json
-    {
-        "commands": [
-            {
-                "symbol": "RandomValue.MaxRandom",
-                "readValue": 1000
-            }
-        ]
-    }
-    ```
-
-1. Get the value that is currently stored in the extension configuration using
-the `RandomValue.MaxRandomFromConfig` symbol
-
-    **Request:**
-
-    ```json
-    {
-        "requestType": "ReadWrite",
-        "commands": [
-            {
-                "symbol": "RandomValue.MaxRandomFromConfig",
-                "commandOptions": [ "SendErrorMessage" ]
-            }
-        ]
-    }
-    ```
-
-    **Response:**
-
-    ```json
-    {
-        "commands": [
-            {
-                "symbol": "RandomValue.MaxRandomFromConfig",
-                "readValue": 10
             }
         ]
     }
